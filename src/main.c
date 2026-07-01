93a6e95da22a290ec0d9e775624b3aa6d63491d6
testxxxxxxxxx <pppppmarcin@gmail.com>
2026-07-01 21:17:34 +0200

    Pass variable to thread

--- src/main.c
+++ src/main.c
@@ -10,30 +10,26 @@
 int main(int argc, char* argv[]) {
 	pthread_t messageThread;
 	MPI_Status status;
-	int provided;
-	/*struct User {
-		int rank, size, lamport;
-		enum STATES {
-			REST,
-			WAIT,
-			HELD
-		} stat;
-		enum MESSAGES {
-			REQUEST,
-			ACK
-		} msg;
-	};*/	
+	int provided;	
 
 	struct User* u = malloc(sizeof(struct User));
 	pthread_mutex_t criticalMut = PTHREAD_MUTEX_INITIALIZER, lamportMut = PTHREAD_MUTEX_INITIALIZER;
 	u->lamport = 0;
+
+	typedef struct {
+		struct User *u;
+		pthread_mutex_t criticalMut;
+		pthread_mutex_t lamportMut;
+	} ThrData;
+
+	ThrData thrData = {u, criticalMut, lamportMut};	
 
 	MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
 
 	MPI_Comm_size(MPI_COMM_WORLD, &u->size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &u->rank);
 
-	pthread_create(&messageThread, NULL, message, 0);
+	pthread_create(&messageThread, NULL, message, &thrData);
 
 	state(u, &criticalMut, &lamportMut);
 
